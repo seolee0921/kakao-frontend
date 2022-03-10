2e1aa15c3deb240d9a46f559fd523dfcaf4c0c3c
seolee0921 <seo050921@gmail.com>
2022-03-10 21:56:19 +0900

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import React, { ChangeEvent, useEffect } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Todo from "./TODO";
+import { Paper, Tab, Tabs } from "@mui/material";
+import PeopleIcon from "@mui/icons-material/People";
+import ChatIcon from "@mui/icons-material/Chat";
 
 const App = (): JSX.Element => {
-  useEffect(() => {
-    alert("hello");
-  }, []);
+  const [state, setState] = useState<string>("friends");
 
+  const changeTab = (value: string) => {
+    setState(value);
+  };
+  console.log(state);
   return (
-    <div className="App">
-      <Todo></Todo>
-    </div>
+    <section>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <Tabs centered variant="fullWidth" value={state}>
+          <Tab
+            icon={<PeopleIcon />}
+            label="친구"
+            value="friends"
+            onClick={() => changeTab("friends")}
+          ></Tab>
+          <Tab
+            icon={<ChatIcon />}
+            label="채팅"
+            value="chats"
+            onClick={() => changeTab("chats")}
+          ></Tab>
+        </Tabs>
+      </Paper>
+    </section>
   );
 };
 
---
+++ src/pages/Test/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Grid, Tabs, Tab } from "@mui/material";
+import useState from "react";
+
+const Test = () => {
+  return (
+    <Tabs>
+      <Tab aria-lebel="친구"></Tab>
+      <Tab aria-lebel="채팅"></Tab>
+    </Tabs>
+  )
+};
+
+export default Test;
